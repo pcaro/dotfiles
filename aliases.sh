bdbf7b0deb0eb354a09957574bcfee285205cc35
pcaro <pcaro@localhost>
2012-08-01 10:46:57 +0200

    Soporte de dotfiles

--- aliases.sh
+++ aliases.sh
@@ -12,3 +12,4 @@ alias grin='grin --follow'
 
 alias sshtyaco='nameTerminal yaco_server; ssh 192.168.13.96'
 alias sshtdemo='nameTerminal yaco_demo;   ssh 82.194.95.94'
+alias dotfiles='dotfiles -C ~/.dotfiles/dotfilesrc'
