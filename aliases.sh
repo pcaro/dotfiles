2b31721fbaca5e38ed75f072ff2809ad31a555bc
pcaro <pcaro@localhost>
2012-08-01 08:54:14 +0200

    cambios en ficheros de configuraci?n

---
+++ aliases.sh
@@ -0,0 +1,14 @@
+
+# some more ls aliases
+alias ll='ls -l -h'
+alias la='ls -A'
+alias l='ls -CF'
+alias rm='rm -i'
+alias cd..='cd ..'
+alias svnst='svn st --ignore-externals'
+alias k='komodo'
+alias s='sublime_text'
+alias grin='grin --follow'
+
+alias sshtyaco='nameTerminal yaco_server; ssh 192.168.13.96'
+alias sshtdemo='nameTerminal yaco_demo;   ssh 82.194.95.94'
